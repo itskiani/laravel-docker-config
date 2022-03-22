7539453e406638c87afda54a597c737ad71a53ee
masoud nazarpoor <m74asoud@gmail.com>
2022-03-22 22:28:59 +0430

    update

--- commands/install.sh
+++ commands/install.sh
@@ -4,7 +4,7 @@ sudo cp ../.env.example ../.env
 sudo docker-compose up --build -d
 echo 'please wait ...'
 sleep 30
-sudo docker-compose run --rm composer install --ignore-platform-reqs
+sudo docker-compose run --rm composer install #--ignore-platform-reqs
 sudo docker-compose run --rm artisan migrate --seed
 sudo docker-compose run php sh -c "chown -R www-data:root ./storage && echo 'permission accept ...' && exit"
 sudo docker-compose run --rm artisan storage:link
